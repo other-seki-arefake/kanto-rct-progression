138d5caa7e6b26a22b4daef1df54a15372edcb54
other-seki-arefake <80777187+other-seki-arefake@users.noreply.github.com>
2026-01-22 14:21:53 -0300

    Add files via upload
    initial commit

---
+++ src/main/java/dev/kanto/KantoModPrueba.java
@@ -0,0 +1,11 @@
+
+package dev.kanto;
+
+import net.neoforged.fml.common.Mod;
+
+@Mod("kanto_mod_prueba")
+public class KantoModPrueba {
+    public KantoModPrueba() {
+        System.out.println("[Kanto] Mod de prueba cargado");
+    }
+}
